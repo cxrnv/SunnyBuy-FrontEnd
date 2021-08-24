4ec0d2b932f81af32b4f0aa129ef665f0c641cb3
Trxuxv <deboraleticiaferreiramaciel@gmail.com>
2021-08-24 18:00:25 -0300

    24/08

---
+++ src/app/pages/payment/payment-type-choose/components/payment-type-header/payment-type-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-type-header',
+  templateUrl: './payment-type-header.component.html',
+  styleUrls: ['./payment-type-header.component.css']
+})
+export class PaymentTypeHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

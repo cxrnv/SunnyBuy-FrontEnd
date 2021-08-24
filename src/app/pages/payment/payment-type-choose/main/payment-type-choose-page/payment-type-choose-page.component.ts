ee4a3a6d84b1e3d45f5f111856e8646766e444ff
Trxuxv <deboraleticiaferreiramaciel@gmail.com>
2021-08-24 17:31:23 -0300

    24/08

---
+++ src/app/pages/payment/payment-type-choose/main/payment-type-choose-page/payment-type-choose-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-type-choose-page',
+  templateUrl: './payment-type-choose-page.component.html',
+  styleUrls: ['./payment-type-choose-page.component.css']
+})
+export class PaymentTypeChoosePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

4676c1cfa0852d45ae8ae785533947bba9cdb31b
Trxuxv <deboraleticiaferreiramaciel@gmail.com>
2021-08-25 11:55:31 -0300

    25/08

---
+++ src/app/pages/payment/creditcard/components/creditcard-header/creditcard-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creditcard-header',
+  templateUrl: './creditcard-header.component.html',
+  styleUrls: ['./creditcard-header.component.css']
+})
+export class CreditcardHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
